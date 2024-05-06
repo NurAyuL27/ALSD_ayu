6c7ddb2fbb9048bc1715f39524116bfd8a5539e5
NurAyuL27 <nurayu2718@gmail.com>
2024-05-06 09:27:55 +0700

    6 5 24

--- minggu10/Queue.java
+++ minggu10/Queue.java
@@ -67,7 +67,7 @@ public class Queue {
     public void Enqueue(int dt) {
         if (IsFull()) {
             System.out.println("Queue sudah penuh");
-            System.exit(dt);
+            System.exit(0);
         } else {
             if (IsEmpty()) {
                 front = rear = 0;
@@ -87,7 +87,7 @@ public class Queue {
         int dt = 0;
         if (IsEmpty()) {
             System.out.println("Queue masih kosong");
-            System.exit(dt);
+            System.exit(0);
         } else {
             dt = data[front];
             size--;
