9dd3579c26ca68fe01d351852e96bd927c537288
NurAyuL27 <nurayu2718@gmail.com>
2024-05-13 12:35:22 +0700

    percobaan 2

--- minggu12/DoubleLinkedList.java
+++ minggu12/DoubleLinkedList.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
-
-import java.util.Scanner;
 public class DoubleLinkedList {
     Node head;
-        int size;
+    int size;
 
     public DoubleLinkedList() {
         head = null;
@@ -88,38 +86,57 @@ public class DoubleLinkedList {
         }
     }
 
-    public class DoubleLinkedLisMain {
-        public static void main(String[] args) {
-    
-            DoubleLinkedList dll = new DoubleLinkedList();
-    
-            dll.print();
-            System.out.println("Size: "+dll.size());
-            System.out.println("===========================================");
-            dll.addFirst(3);
-            dll.addLast(4);
-            dll.addFirst(7);
-    
-            dll.print();
-            System.out.println("Size: "+dll.size());
-            System.out.println("===========================================");
-            try {
-            dll.add(40, 1);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-    
-            dll.print();
-            System.out.println("Size: "+dll.size());
-            System.out.println("===========================================");
-            dll.clear();
-    
-            dll.print();
-            System.out.println("Size: "+dll.size());
+    public void removeFirst() throws Exception {
+            if (isEmpty()) {
+                throw new Exception("Linked List masih kosong, tidak dapat dihapus!");
+            } else if (size == 1) {
+                removeLast();
+            } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
 
-            System.out.println("-------------------------------------------");
-            System.out.println("BUILD SUCCESS");
-            System.out.println("-------------------------------------------");
-        } 
+    public void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+            Node current = head;
+            while (current.next.next!= null) {
+                current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+
+    public void remove (int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas");
+        } else if (index == 0) {
+            removeFirst();
+        } else {
+            Node current = head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.next == null) {
+                current.prev.next = null;
+            } else if (current.prev == null) {
+                current = current.next;
+                current.prev = null;
+                head = current;
+            } else {
+                current.prev.next = current.next; 
+                current.next.prev = current.prev;
+            }
+            size--;
+        }
     }
 }
